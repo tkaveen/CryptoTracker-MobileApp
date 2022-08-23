1db3a551daf3d47447bbe9db2cd55a376926d79a
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2022-08-23 22:40:01 +0530

    Basic row component done

--- App.js
+++ App.js
@@ -1,24 +1,81 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image
-        source={{
-          uri: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png",
-        }}
-        style={{ height: 30, width: 30 }}
-      />
-      <View>
-        <Text style={styles.title}>Bitcoin</Text>
-        <View style={{ flexDirection: "row" }}>
-          <Text style={styles.text}>1</Text>
-          <Text style={styles.text}>BTC</Text>
-          <Text style={styles.text}>0.63%</Text>
+      {/* 1 ROW */}
+      <View style={styles.coinContainer}>
+        <Image
+          source={{
+            uri: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png",
+          }}
+          style={{
+            height: 30,
+            width: 30,
+            marginRight: 10,
+            alignSelf: "center",
+          }}
+        />
+        <View>
+          <Text style={styles.title}>Bitcoin</Text>
+          <View style={{ flexDirection: "row" }}>
+            <View style={styles.rankContainer}>
+              <Text style={styles.rank}>1</Text>
+            </View>
+            <Text style={styles.text}>BTC</Text>
+            <AntDesign
+              name="caretdown"
+              size={12}
+              color="white"
+              style={{ alignSelf: "center", marginRight: 5 }}
+            />
+            <Text style={styles.text}>0.63%</Text>
+          </View>
+        </View>
+        <View style={{ marginLeft: "auto" }}>
+          <Text style={styles.title}>23.056,30</Text>
+          <Text style={styles.title}>MP 1.0765 T</Text>
         </View>
       </View>
+
+      {/* 2ROW */}
+      <View style={styles.coinContainer}>
+        <Image
+          source={{
+            uri: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png",
+          }}
+          style={{
+            height: 30,
+            width: 30,
+            marginRight: 10,
+            alignSelf: "center",
+          }}
+        />
+        <View>
+          <Text style={styles.title}>Bitcoin</Text>
+          <View style={{ flexDirection: "row" }}>
+            <View style={styles.rankContainer}>
+              <Text style={styles.rank}>1</Text>
+            </View>
+            <Text style={styles.text}>BTC</Text>
+            <AntDesign
+              name="caretdown"
+              size={12}
+              color="white"
+              style={{ alignSelf: "center", marginRight: 5 }}
+            />
+            <Text style={styles.text}>0.63%</Text>
+          </View>
+        </View>
+        <View style={{ marginLeft: "auto" }}>
+          <Text style={styles.title}>23.056,30</Text>
+          <Text style={styles.title}>MP 1.0765 T</Text>
+        </View>
+      </View>
+
       <StatusBar style="light" />
     </View>
   );
@@ -34,8 +91,27 @@ const styles = StyleSheet.create({
     color: "white",
     fontSize: 16,
     fontWeight: "bold",
+    marginBottom: 3,
   },
   text: {
     color: "white",
+    marginRight: 5,
+  },
+  coinContainer: {
+    flexDirection: "row",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: "gray",
+    padding: 15,
+    justifyContent: "",
+  },
+  rank: {
+    fontWeight: "bold",
+    color: "white",
+  },
+  rankContainer: {
+    backgroundColor: "#585858",
+    paddingHorizontal: 5,
+    borderRadius: 5,
+    marginRight: 5,
   },
 });
